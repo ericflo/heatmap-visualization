b02d18c3bc2665215458c19174fa1af8aa8ade30
ericflo <floguy@gmail.com>
2022-11-14 22:50:51 +0000

    Deploying to public from @ ericflo/heatmap-visualization@cd37e41e4ee4375b64d259acfdf6cbdcf16695a7 🚀

--- _next/static/xxtEiQDvrxNPvUJZov3YB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fb7d5399-dc37c3f4e80b4135.js","static/chunks/983-741e4b4925bb3af1.js","static/chunks/pages/index-f2bf5fc36b8f59a7.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
