c1da8de3364c00b2dadd170f38d129cec04e5bf7
Eric Florenzano <eric.florenzano@shopify.com>
2022-11-14 18:22:56 -0800

    Drop all the image loading stuff, use different glb for heatmap

--- components/HeatmapModel.tsx
+++ components/HeatmapModel.tsx
@@ -1,38 +1,15 @@
 import * as THREE from "three";
-import { useRef, useEffect } from "react";
-import { useLoader } from "@react-three/fiber";
+import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 interface HeatmapModelProps {
   assetPrefix: string;
-  wireframe?: boolean;
 }
 
-const HeatmapModel = ({ assetPrefix, wireframe }: HeatmapModelProps) => {
+const HeatmapModel = ({ assetPrefix }: HeatmapModelProps) => {
   const group = useRef<THREE.Group>(null!);
-  const glbPath = assetPrefix + "/models/rebuilt shop heatmap 04 8k.glb";
-  const texPath = assetPrefix + "/textures/meredith_map flattened 02 2K.png";
-  const texAlphaPath =
-    assetPrefix + "/textures/meredith_map flattened 02 2K alpha.png";
-  //const texPath = assetPrefix + "/textures/new_tex.png";
+  const glbPath = assetPrefix + "/models/heatmap.glb";
   const { nodes } = useGLTF(glbPath);
-  const colorMap = useLoader(TextureLoader, texPath);
-  const alphaMap = useLoader(TextureLoader, texAlphaPath);
-  useEffect(() => {
-    if (colorMap) {
-      colorMap.flipY = false;
-      colorMap.generateMipmaps = true;
-      colorMap.minFilter = THREE.NearestFilter;
-      colorMap.magFilter = THREE.NearestFilter;
-    }
-    if (alphaMap) {
-      alphaMap.flipY = false;
-      alphaMap.generateMipmaps = true;
-      alphaMap.minFilter = THREE.NearestFilter;
-      alphaMap.magFilter = THREE.NearestFilter;
-    }
-  }, [colorMap, alphaMap]);
   return (
     <group ref={group} dispose={null}>
       {Object.keys(nodes).map((key, i) => {
@@ -40,35 +17,20 @@ const HeatmapModel = ({ assetPrefix, wireframe }: HeatmapModelProps) => {
         if (!node.geometry) {
           return null;
         }
-        const mat = new THREE.MeshBasicMaterial({
-          transparent: true,
-          map: colorMap,
-          depthWrite: false,
-        });
-        const wireframeMat = wireframe
-          ? new THREE.MeshBasicMaterial({
-              transparent: false,
-              wireframe: true,
-              color: 0xffffff,
-            })
-          : undefined;
+        const basicMat = node.material as THREE.MeshBasicMaterial;
         return (
-          <>
-            <mesh
-              key={key}
-              geometry={node.geometry}
-              material={mat}
-              renderOrder={1}
-            />
-            {wireframe ? (
-              <mesh
-                key={key}
-                geometry={node.geometry}
-                material={wireframeMat}
-                renderOrder={2}
-              />
-            ) : null}
-          </>
+          <mesh
+            key={key}
+            geometry={node.geometry}
+            material={
+              new THREE.MeshBasicMaterial({
+                transparent: true,
+                map: basicMat.map,
+                depthWrite: false,
+              })
+            }
+            renderOrder={1}
+          />
         );
       })}
     </group>
