6520b10e34a9a9638288117f2e48151ca1896935
Eric Florenzano <eric.florenzano@shopify.com>
2022-11-14 17:21:02 -0800

    Fix issues with z-fighting and work on uv island issue

--- components/HeatmapModel.tsx
+++ components/HeatmapModel.tsx
@@ -6,19 +6,33 @@ import { TextureLoader } from "three/src/loaders/TextureLoader";
 
 interface HeatmapModelProps {
   assetPrefix: string;
+  wireframe?: boolean;
 }
 
-const HeatmapModel = ({ assetPrefix }: HeatmapModelProps) => {
+const HeatmapModel = ({ assetPrefix, wireframe }: HeatmapModelProps) => {
   const group = useRef<THREE.Group>(null!);
   const glbPath = assetPrefix + "/models/rebuilt shop heatmap 04 8k.glb";
   const texPath = assetPrefix + "/textures/meredith_map flattened 02 2K.png";
+  const texAlphaPath =
+    assetPrefix + "/textures/meredith_map flattened 02 2K alpha.png";
   //const texPath = assetPrefix + "/textures/new_tex.png";
   const { nodes } = useGLTF(glbPath);
   const colorMap = useLoader(TextureLoader, texPath);
+  const alphaMap = useLoader(TextureLoader, texAlphaPath);
   useEffect(() => {
-    colorMap.flipY = false;
-    colorMap.generateMipmaps = false;
-  }, [colorMap]);
+    if (colorMap) {
+      colorMap.flipY = false;
+      colorMap.generateMipmaps = true;
+      colorMap.minFilter = THREE.NearestFilter;
+      colorMap.magFilter = THREE.NearestFilter;
+    }
+    if (alphaMap) {
+      alphaMap.flipY = false;
+      alphaMap.generateMipmaps = true;
+      alphaMap.minFilter = THREE.NearestFilter;
+      alphaMap.magFilter = THREE.NearestFilter;
+    }
+  }, [colorMap, alphaMap]);
   return (
     <group ref={group} dispose={null}>
       {Object.keys(nodes).map((key, i) => {
@@ -26,17 +40,35 @@ const HeatmapModel = ({ assetPrefix }: HeatmapModelProps) => {
         if (!node.geometry) {
           return null;
         }
-        const mat = node.material as THREE.MeshStandardMaterial;
-        mat.map = colorMap;
+        const mat = new THREE.MeshBasicMaterial({
+          transparent: true,
+          map: colorMap,
+          depthWrite: false,
+        });
+        const wireframeMat = wireframe
+          ? new THREE.MeshBasicMaterial({
+              transparent: false,
+              wireframe: true,
+              color: 0xffffff,
+            })
+          : undefined;
         return (
-          <mesh
-            key={key}
-            castShadow
-            receiveShadow
-            geometry={node.geometry}
-            material={mat}
-            renderOrder={1}
-          />
+          <>
+            <mesh
+              key={key}
+              geometry={node.geometry}
+              material={mat}
+              renderOrder={1}
+            />
+            {wireframe ? (
+              <mesh
+                key={key}
+                geometry={node.geometry}
+                material={wireframeMat}
+                renderOrder={2}
+              />
+            ) : null}
+          </>
         );
       })}
     </group>
